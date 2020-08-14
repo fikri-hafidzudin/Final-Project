bdc63d17cd3e68448a02b56b22eb913902f875c3
dumblek <duharamadhani@gmail.com>
2020-08-14 18:17:09 +0700

    Merge branch 'master' of https://github.com/fikri-hafidzudin/Final-Project into master

--- resources/views/pertanyaan/create.blade.php
+++ resources/views/pertanyaan/create.blade.php
@@ -1,11 +1,6 @@
 @extends('pertanyaan.blank')
-
-@push('script-head')
-<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-@endpush
-
 @section('content')
-    <form role="form" action="/pertanyaan" method="POST">
+    <form role="form" action="{{url ('/pertanyaanbaru') }}" method="POST">
       @csrf
       <div class="mt-3 ml-3 mr-3">
           <div class="card card-primary">
@@ -23,8 +18,7 @@
             </div>
             <div class="form-group">
               <label for="isi">Isi</label>
-              {{-- <input type="text" class="form-control" id="isi" name="isi" value="{{old('isi', '')}}" placeholder="Pertanyaan"> --}}
-              <textarea name="isi" class="form-control my-editor" >{!! old('isi', '') !!}</textarea>
+              <input type="text" class="form-control" id="isi" name="isi" value="{{old('isi', '')}}" placeholder="Pertanyaan">
             @error('isi')
             <div class="alert alert-danger">{{$message}}</div>
             @enderror
@@ -38,43 +32,5 @@
       </div>
     </form>
 
+
 @endsection
-
-@push('script')
-    <script>
-      var editor_config = {
-        path_absolute : "/",
-        selector: "textarea.my-editor",
-        plugins: [
-          "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-          "searchreplace wordcount visualblocks visualchars code fullscreen",
-          "insertdatetime media nonbreaking save table contextmenu directionality",
-          "emoticons template paste textcolor colorpicker textpattern"
-        ],
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-        relative_urls: false,
-        file_browser_callback : function(field_name, url, type, win) {
-          var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-          var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-          var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-          if (type == 'image') {
-            cmsURL = cmsURL + "&type=Images";
-          } else {
-            cmsURL = cmsURL + "&type=Files";
-          }
-
-          tinyMCE.activeEditor.windowManager.open({
-            file : cmsURL,
-            title : 'Filemanager',
-            width : x * 0.8,
-            height : y * 0.8,
-            resizable : "yes",
-            close_previous : "no"
-          });
-        }
-      };
-      
-      tinymce.init(editor_config);
-    </script>
-@endpush
