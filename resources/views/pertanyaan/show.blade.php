f914529cb8cc67495971b401617b2ad594560646
dumblek <duharamadhani@gmail.com>
2020-08-14 20:13:18 +0700

    edit text area jawaban

--- resources/views/pertanyaan/show.blade.php
+++ resources/views/pertanyaan/show.blade.php
@@ -1,4 +1,9 @@
 @extends('pertanyaan.blank')
+
+@push('script-head')
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+@endpush
+
 @section('content')
 <div class="mt-3 ml-3">
   <div class="card"> 
@@ -26,7 +31,7 @@
               <tbody>
               @forelse ($pertanyaan->jawaban as $key => $jawab)
                 <tr>
-                    <td> {{$jawab -> isi}} </td>
+                    <td> {!! $jawab -> isi !!} </td>
                     <td style="display: flex;">
                         <a href="{{ route('jawaban.edit', $jawab->id) }}" class="btn btn-warning btn-sm mr-1 ml-1">edit</a>
                         <form action="{{ route('jawaban.destroy', $jawab->id) }}" method="POST">
@@ -50,7 +55,7 @@
           <div class="card-body">
             <div class="form-group">
               <label for="isi">Jawab</label>
-              <input type="text" class="form-control" id="isi" name="isi" value="{{old('isi', '')}}" placeholder="Jawaban">
+              <textarea name="isi" class="form-control my-editor">{!! old('isi', '') !!}</textarea>
             @error('isi')
             <div class="alert alert-danger">{{$message}}</div>
             @enderror
@@ -75,3 +80,42 @@
 </div>
     
 @endsection
+
+@push('script')
+<script>
+  var editor_config = {
+    path_absolute : "/",
+    selector: "textarea.my-editor",
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
+</script>
+@endpush
